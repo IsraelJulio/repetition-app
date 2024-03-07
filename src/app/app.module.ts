4f413a27b051c08a56954db1b69988f2efc78174
ISRAEL JULIO <ISRAEL_JULIO@jabil.com>
2024-03-07 16:14:35 -0300

    install prime ng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ButtonModule } from 'primeng/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuizComponent } from './quiz/quiz.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    QuizComponent
-  ],
+  declarations: [AppComponent, QuizComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ButtonModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
